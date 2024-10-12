8339e9c03f24638c96982feafcf26253e0ad546e
KimADR <andriamahatoky.mael2020@gmail.com>
2024-10-12 21:37:43 +0300

    starting the main page

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div className="h-screen flex">
+          {/* LEFT */}
+          <div className="w-[14%] bg-red-50">l</div>
+          {/* RIGHT */}
+          <div className="w-[86%] bg-blue-200">r</div>
+        </div>
+      );
+  }
+  
