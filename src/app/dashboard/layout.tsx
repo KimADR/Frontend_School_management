999d52499d36df0509c94d293856268df18dbcdc
KimADR <andriamahatoky.mael2020@gmail.com>
2024-10-12 21:05:51 +0300

    page created

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div>
+          dashboard
+          {children}
+        </div>
+      );
+  }
+  
