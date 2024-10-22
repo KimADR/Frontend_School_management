81c9940763430df9389fa0095394b4beac17903b
KimADR <andriamahatoky.mael2020@gmail.com>
2024-10-22 22:51:57 +0300

    starting to create list/teacher page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,7 +15,7 @@ const menuItems = [
       {
         icon: "/teacher.png",
         label: "Teachers",
-        href: "/teachers",
+        href: "/list/teachers",
         visible: ["admin", "teacher"],
       },
       {
